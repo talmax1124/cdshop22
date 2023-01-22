ee7776d0959b328f286907079a88c2c7a764c34d
Carlos <carlitosdiazplaza@gmail.com>
2023-01-22 13:03:44 -0500

    ok

--- backend/server.js
+++ backend/server.js
@@ -94,14 +94,6 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: ["Content-Type" /*, "Authorization"*/],
-};
-
-app.use(cors(corsOptions));
 
 app.use(notFound);
 app.use(errorHandler);
