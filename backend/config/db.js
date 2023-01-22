7b48fb4601fc54ac6b0b97091a5f90830ebf3af0
Carlos <carlitosdiazplaza@gmail.com>
2023-01-22 00:26:12 -0500

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       // useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
