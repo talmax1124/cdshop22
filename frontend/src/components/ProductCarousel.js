32253e6dced732001fa37588488d9e8fd60a959a
Carlos <carlitosdiazplaza@gmail.com>
2023-02-13 12:23:10 -0500

    Update

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Image } from "react-bootstrap";
+import { Image, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "./Loader";
 import Message from "./Message";
@@ -21,20 +21,30 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant="danger">{error}</Message>
   ) : (
-    <div className="flex justify-between top-products">
-      {products.map((product) => (
-        <div key={product._id} className="bg-slate-100 mr-2 ml-2 p-3 top-card rounded-md">
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
-            <p>
-              <h2 className="font-bold mt-3 mb-">{product.name}</h2>
+    <>
+      <div className="flex justify-between top-products">
+        {products.map((product) => (
+          <div
+            key={product._id}
+            className="bg-slate-100 mr-2 ml-2 p-3 top-card rounded-md"
+          >
+            <Link to={`/product/${product._id}`}>
+              <Image src={product.image} alt={product.name} fluid />
+              <p>
+                <h2 className="font-bold mt-3 mb-">{product.name}</h2>
 
-              <h2 className="font-medium mt-1">$ {product.price}</h2>
-            </p>
-          </Link>
-        </div>
-      ))}
-    </div>
+                <h2 className="font-medium mt-1">$ {product.price}</h2>
+              </p>
+            </Link>
+          </div>
+        ))}
+      </div>
+      <Link to="/products">
+        <Button className="text-black bg-slate-50   rounded-lg mb-3 mt-2 no-underline hover:no-underline">
+          View All Products
+        </Button>
+      </Link>
+    </>
   );
 };
 
